ba433762c1f773658321c55b71e5f60d9d2a3b9a
Yu-Cheng Liang <a135246a135246@gmail.com>
2016-11-02 20:14:30 +0800

    add pointer_add.c

---
+++ pointer_add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int a = 3;
+	char c = '3';
+
+	int *intptr = &a;
+	char *charptr = &c;
+
+
+	//set breakpoint here
+	//(gdb) b 12
+	return 0;
+}
