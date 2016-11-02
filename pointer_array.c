adec9f3139969c05e036fbc5de5c1108ac5ecab6
Yu-Cheng Liang <a135246a135246@gmail.com>
2016-11-02 21:19:38 +0800

    add pointer_array.c

---
+++ pointer_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int arr[10] = {1,1,2,3,5,8,13,21,34,55};
+
+	int i;
+	for (i = 0; i < 10; i++) {
+		/*
+		your code to print out arr, don't use arr[]
+		*/
+	}
+	
+	return 0;
+}
